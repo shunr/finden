75037c4f9c39b0f5ca03a4b28d4c3bca44535979
Ali Nikseresht <ali.nikseresht21@gmail.com>
2017-09-17 04:27:08 -0400

    site tweaks and added leaderboard endpoint

--- server/colander/routes/leaderboard.js
+++ server/colander/routes/leaderboard.js
@@ -4,11 +4,17 @@ const router = express.Router();
 const db = require('../colander/db');
 
 /* GET leaderboard page. */
-router.get('/leaderboard', function(req, res, next) {
+router.get('/leaderboard', (req, res, next) =>{
   db.getLeaderboard().then((data)=>{
     res.render('leaderboard', { title: 'Finden', leaderboardData: data });
   });
   
+});
+
+router.get('/leaderboardjson', (req, res, next)=>{
+  db.getLeaderboard().then((data)=>{
+    res.send(data);
+  });
 });
 
 module.exports = router;
